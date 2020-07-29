512fe3742e0c9a0ba92f86cb0bd2eabc554830e9
Rodrigo-Sarmento <59851576+Rodrigo-Sarmento@users.noreply.github.com>
2020-07-28 21:55:54 -0300

    commit carousel

--- src/pages/cadastro/categoria/index.js
+++ src/pages/cadastro/categoria/index.js
@@ -2,19 +2,31 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import PageDefault from '../../../components/PageDefault';
 
-function CadastroCategoria(){
-    return (
-      <PageDefault>
-        <h1>cadastro de categoria</h1>
+function CadastroCategoria() {
+  return (
+    <PageDefault>
+      <h1>Cadastro de Categoria</h1>
+
+      <form>
+
+        <label>
+          Nome da Categoria:
+          <input
+            type="text"
+          />
+        </label>
+
+        <button>
+          Cadastrar
+        </button>
+      </form>
 
 
+      <Link to="/">
+        Ir para home
+      </Link>
+    </PageDefault>
+  )
+}
 
-        
-        <Link to="/">
-          ir para home
-        </Link>
-      </PageDefault>
-    )
-  }
-
-  export default CadastroCategoria;
+export default CadastroCategoria;
